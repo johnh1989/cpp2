b7f76f14491ce21da8a54b87fd0a9723bfc2908d
john <jh011889@outlook.com>
2015-04-05 12:26:17 -0400

    updates to assignment 6

--- assignment6/main.cpp
+++ assignment6/main.cpp
@@ -4,13 +4,14 @@
 #include <Limits>
 
 /*
-Programming Assignment 6 is due Wednesday April 8.  Use the BinaryTree and BinarySearchTree class files posted on the Angel site under the Programming Assignment 6 folder under the Programming Assignments folder to write the following program:
+Programming Assignment 6 is due Wednesday April 8.  Use the BinaryTree and BinarySearchTree class files posted on the
+Angel site under the Programming Assignment 6 folder under the Programming Assignments folder to write the following program:
 
     You will process Employee objects in the Binary Search Tree
     Use the Employee class you created with project 1
-        You will need to overload the "<" and ">" operators in the Employee class as these operations are used in the Binary Search Tree
-            Use the Employee Number data member in defining the "<" and ">" operators
-        Update the "==" operator overload in the Employee class to use the Employee Number rather than Last Name
+    You will need to overload the "<" and ">" operators in the Employee class as these operations are used in the Binary Search Tree
+    Use the Employee Number data member in defining the "<" and ">" operators
+    Update the "==" operator overload in the Employee class to use the Employee Number rather than Last Name
 
 Write the following program:
 
@@ -57,9 +58,13 @@ int main(){
                     break;
 
             case 5: cout << endl;
-                    cout << "\nAll Employees" << endl;
-                    cout << " ";
-                    tree.inorderTraversal();
+                    if (tree.isEmpty()){
+                        cout << "\nTree is empty"  << endl;
+                    }else{
+                        cout << "\nAll Employees" << endl;
+                        cout << " ";
+                        tree.inorderTraversal();
+                    }
                     break;
         }
 	}
@@ -97,9 +102,11 @@ void addEmployee(bSearchTreeType<Employee>& tree){
 
     cout << "\nEnter gender: m/f: ";
     cin >> gender;
-    while (gender != 'm' && gender != 'M' && gender != 'f' && gender != 'F'){
+    gender = toupper(gender);
+    while (gender != 'M' && gender != 'F'){
         cout << "\nPlease enter m or f: ";
         cin >> gender;
+        gender = toupper(gender);
     }
 
     /*Create temporary employee object
@@ -113,29 +120,38 @@ void addEmployee(bSearchTreeType<Employee>& tree){
 void deleteEmployee(bSearchTreeType<Employee>& tree){
     Employee temp;
     int employeeNumber;
-    bool found = false;
-    cout << " \nList of all Employees..." << endl;
-    cout << " ";
-    tree.inorderTraversal();
-    cout << endl;
-
-    cout << "Enter employee number to delete ";
-    cin >> employeeNumber;
-    temp.SetemployeeNumber(employeeNumber);
-    found = tree.search(temp);
-    if (found){
-        tree.deleteNode(temp);
+    bool found;
+    if (tree.isEmpty()){
+        cout << "\nTree is empty" << endl;
     }else{
-        cout << "\nCould find that employee..." << endl;
+        cout << " \nList of all Employees..." << endl;
+        cout << " ";
+        tree.inorderTraversal();
+        cout << endl;
+
+
+        cout << "Enter employee number to delete ";
+        cin >> employeeNumber;
+        temp.SetemployeeNumber(employeeNumber);
+        found = tree.search(temp);
+        if (found){
+            tree.deleteNode(temp);
+        }else{
+            cout << "\nCould find that employee..." << endl;
+        }
     }
 }
 
 void searchForEmployee(bSearchTreeType<Employee>& tree){
     int employeeNumber;
     Employee temp;
-    cout << "Enter the employee number you would like to search for: ";
-    cin >> employeeNumber;
-    temp.SetemployeeNumber(employeeNumber);
-    tree.printElement(temp);
+    if (tree.isEmpty()){
+        cout << "\nTree is empty" << endl;
+    }else{
+        cout << "Enter the employee number you would like to search for: ";
+        cin >> employeeNumber;
+        temp.SetemployeeNumber(employeeNumber);
+        tree.printElement(temp);
+    }
 }
 
